8300dadaa04ccb877b6c1ae5a9f61c1bb340c96e
StvAlxnder <smamanie@unjbg.edu.pe>
2025-07-09 19:04:07 -0500

    Mejora de interfaz de Ejercicio 02.cpp

--- Ejercicio 02.cpp
+++ Ejercicio 02.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main (){
-	int n, S=0, A[25], prom;
-	cout<<"Ingresa la cantidad de numeros: ";
-	cin>>n;
+	int n, S=0, prom;
+	system("cls");
+	cout<<":::::::::: Detector de numeros mayores al promedio ::::::::::"<<endl;
+	cout<<"Ingresa la cantidad de numeros a ingresar: "; cin>>n;
+	int A[n];
 	for (int i=0;i<n;i++){
-		cout<<"Ingresa el numero: "<<endl;
-		cin>>A[i];
+		cout<<"Ingresa el numero "<<i+1<<": "; cin>>A[i];
 		S=S+A[i];
 	}
 	prom=S/n;
-	cout<<"Numeros mayores al promedio: "<<endl;
+	cout<<"\nNumeros mayores a "<<prom<<": "<<endl;
 	for(int i=0;i<n;i++){
 		if (A[i]>prom){
-			cout<<A[i]<<endl;
+			cout<<left<<setw(10)<<A[i]<<endl;
 		}
 	}
+	system("pause");
 	return 0;
 }
